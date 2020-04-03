566f1489c3ad81eb1ed76e101f9d43cbd4ea1a88
Hung Le <leviethung2403@gmail.com>
2020-04-02 17:40:43 -0700

    add set field function

--- getstart_button/signals.py
+++ getstart_button/signals.py
@@ -9,9 +9,8 @@ def getstart_button_model_post_save(sender, instance, **kwargs):
     page_id = instance.page_id
     welcome_configs = sender.objects.get(page_id=page_id,)
     print(welcome_configs.payload)
-    response_message = {
-        'get_started': {
-            'payload': welcome_configs.payload
-        },
-    }
-    views.call_send_api(page_id, response_message)
+    views.set_field(
+        page_id,
+        'get_started',
+        welcome_configs.payload
+    )
