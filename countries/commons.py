88cbab185a911007ea50b72f6e0b2b19e7b5a0c0
xuannghia <xuannghia197@gmail.com>
2020-04-05 02:02:02 +0700

    :vv

--- countries/commons.py
+++ countries/commons.py
@@ -14,11 +14,7 @@ def fetch_new_statistics():
         data = json.loads(content)['data']
         for c in data['TG']:
             try:
-                search = pycountry.countries.search_fuzzy(c['province_name'])
-                if len(search) == 0:
-                    raise Exception('')
-                code = search[0].alpha_2
-                name = '{} {}'.format(c['province_name'], flag.flag(code))
+                province_name = c['province_name']
                 cases = c['confirmed']
                 death = c['deaths']
                 recovered = c['recovered']
@@ -27,19 +23,27 @@ def fetch_new_statistics():
                 death_today = c['increase_deaths']
                 recovered_today = c['increase_recovered']
 
-                if c['province_name'] == 'Thế giới':
+                if province_name == 'Thế giới':
                     code = 'TE'
                     name = 'Thế Giới'
-                elif c['province_name'] == 'Vietnam':
+                else:
+                    search = pycountry.countries.search_fuzzy(province_name)
+                    if len(search) == 0:
+                        raise Exception('')
+                    code = search[0].alpha_2
+                    name = '{} {}'.format(province_name, flag.flag(code))
+
+                if province_name == 'Vietnam':
                     name = 'Việt Nam {}'.format(flag.flag('VN'))
-                elif c['province_name'] == 'USA':
+                elif province_name == 'USA':
                     name = 'Mỹ {}'.format(flag.flag('US'))
-                elif c['province_name'] == 'China':
+                elif province_name == 'China':
                     name = 'Trung Quốc {}'.format(flag.flag('CN'))
-                elif c['province_name'] == 'Russia':
+                elif province_name == 'Russia':
                     name = 'Nga {}'.format(flag.flag('RU'))
-                elif c['province_name'] == 'Japan':
+                elif province_name == 'Japan':
                     name = 'Nhật Bản {}'.format(flag.flag('JP'))
+
                 print(code)
                 country, created = Country.objects.update_or_create(
                     code=code,
