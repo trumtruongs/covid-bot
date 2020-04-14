425cdd2beef672fa9a4413264907121b469ea4c7
xuannghia <xuannghia197@gmail.com>
2020-04-14 09:50:39 +0700

    Update admin Post

--- posts/admin.py
+++ posts/admin.py
@@ -6,5 +6,7 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('type', 'link', 'title', 'message', 'created_at', 'updated_at')
+    list_display = ('title', 'link', 'type', 'message', 'created_at', 'updated_at')
     save_on_top = True
+    search_fields = ('type', 'title')
+    list_filter = ('type',)
