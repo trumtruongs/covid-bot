1a73a0011598b7ec7b1370e69844bd75bd46f005
xuannghia <xuannghia197@gmail.com>
2020-04-14 15:13:11 +0700

    Update Filter, Actions

--- patients/admin.py
+++ patients/admin.py
@@ -5,3 +5,12 @@ from patients.models import Patient
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('code', 'is_healthy', 'gender', 'year_of_birth', 'address', 'detail')
+    list_filter = ('is_healthy', 'gender')
+    search_fields = ('code',)
+    actions = ('is_healthy', 'is_not_healthy',)
+
+    def is_healthy(self, request, qs):
+        qs.update(is_healthy=True)
+
+    def is_not_healthy(self, request, qs):
+        qs.update(is_healthy=True)
